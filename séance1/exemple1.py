e107fc848621ae6ebdcd5618c7369f88253f4a9f
Ahmed Shili <angobtef@protonmail.com>
2024-02-01 14:46:12 +0100

    exemple1 : variables, type de base, print

---
+++ séance1/exemple1.py
@@ -0,0 +1,9 @@
+nom="Ali" # variable de type str
+age=30 # variable de type int
+salaire=1200.7645 # variable de type floatt
+est_celibataire=True # variable de type booléen7
+# Afficher les informations de la personne
+# On appelle print
+# print("hello","world",sep=";",end="|")
+print('L\'employé "',nom, "\" a comme salaire ",salaire,
+      " et comme age ",age," ,celibataire:",est_celibataire, sep="")
